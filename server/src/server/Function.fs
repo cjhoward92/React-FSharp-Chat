8c39330f9ca2a78fc2d61a3e7ccfa47760264ad4
Carson Howard <cjhoward92@gmail.com>
2019-11-06 23:35:53 -0700

    Added more code and stuff

--- server/src/server/Function.fs
+++ server/src/server/Function.fs
@@ -11,18 +11,70 @@ open System.Net
 ()
 
 type Functions() =
-    /// <summary>
-    /// A Lambda function to respond to WS $connect route from API Gateway
-    /// </summary>
-    /// <param name="request"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
+    static member val monitor: obj = System.Object() with get
+    member val connections: list<string> = [] with get, set
     member __.OnConnect (request: APIGatewayProxyRequest) (context: ILambdaContext) =
         sprintf "Request: %s" request.Path
         |> context.Logger.LogLine
+
+        lock Functions.monitor (fun () -> __.connections <- request.RequestContext.ConnectionId :: __.connections)
 
         let msg = sprintf "Hello from AWS F#! ConnectionId: %s" request.RequestContext.ConnectionId
         APIGatewayProxyResponse(
             StatusCode = int HttpStatusCode.OK,
             Body = JsonConvert.SerializeObject({| data = msg |})
         )
+
+    member __.OnDisconnect (request: APIGatewayProxyRequest) (context: ILambdaContext) =
+        sprintf "Request: %s" request.Path
+        |> context.Logger.LogLine
+
+        lock Functions.monitor (fun () -> __.connections <- List.filter (fun c -> c = request.RequestContext.ConnectionId) __.connections)
+
+        let msg = sprintf "Disconnecting... ConnectionId: %s" request.RequestContext.ConnectionId
+        APIGatewayProxyResponse(
+            StatusCode = int HttpStatusCode.OK,
+            Body = JsonConvert.SerializeObject({| data = msg |})
+        )
+
+    member __.SendMessage (request: APIGatewayProxyRequest) (context: ILambdaContext) =
+        sprintf "Request: %s" request.Path
+        |> context.Logger.LogLine
+
+        sprintf "Parsing" |> context.Logger.LogLine
+        let body = JsonConvert.DeserializeObject<{| data: obj; |}>(request.Body)
+        let dataText = JsonConvert.SerializeObject(body.data)
+
+        sprintf "Serialized and ready to load" |> context.Logger.LogLine
+        use memStream = new System.IO.MemoryStream()
+        use streamWriter = new System.IO.StreamWriter(memStream, System.Text.Encoding.UTF8, dataText.Length, true)
+        use management = new Amazon.ApiGatewayManagementApi.AmazonApiGatewayManagementApiClient(Amazon.RegionEndpoint.USWest2)
+        let postReq = Amazon.ApiGatewayManagementApi.Model.PostToConnectionRequest()
+        postReq.ConnectionId <- request.RequestContext.ConnectionId
+        
+        sprintf "About to start async" |> context.Logger.LogLine
+        let result =
+            async {
+                do sprintf "Writing to mem stream" |> context.Logger.LogLine
+                do! streamWriter.WriteAsync(dataText) |> Async.AwaitTask
+                do! streamWriter.FlushAsync() |> Async.AwaitTask
+                do memStream.Position <- 0L
+                do postReq.Data <- memStream
+                do sprintf "Sending...." |> context.Logger.LogLine
+                return! management.PostToConnectionAsync postReq |> Async.AwaitTask
+            } |> Async.RunSynchronously
+
+        sprintf "Operation complete" |> context.Logger.LogLine
+        match result.HttpStatusCode with
+        | HttpStatusCode.OK ->
+            let msg = sprintf "Hello from AWS F#! ConnectionId: %s" request.RequestContext.ConnectionId
+            APIGatewayProxyResponse(
+                StatusCode = int HttpStatusCode.OK,
+                Body = JsonConvert.SerializeObject({| data = msg |})
+            )
+        | _ ->
+            let msg = sprintf "FAILURE! ConnectionId: %s" request.RequestContext.ConnectionId
+            APIGatewayProxyResponse(
+                StatusCode = int HttpStatusCode.BadRequest,
+                Body = JsonConvert.SerializeObject({| data = msg |})
+            )
